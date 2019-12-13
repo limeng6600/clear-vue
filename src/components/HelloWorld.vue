57ab44c5340b3745197e4d467b051380c941aa05
unknown <229898977@qq.com>
2019-12-13 11:38:13 +0800

    初始项目提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      {{msg}}
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      data(){
+        return {
+          msg:'helloWorld'
+        }
+      }
+    }
+</script>
